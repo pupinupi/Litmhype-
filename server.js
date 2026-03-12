05a217ee2a259a8cbb83c54750407d80a8f41b76
pupinupi <cool.lg-lg@yandex.ru>
2026-03-12 17:04:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,58 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const express = require("express")
+const app = express()
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-let rooms = {};
+let rooms = {}
 
 io.on("connection", (socket) => {
 
-    socket.on("joinRoom", (data) => {
+    socket.on("joinRoom", data => {
+        const {name, room, color} = data
 
-        const {room, name, color} = data;
+        if (!rooms[room]) rooms[room] = {players: [], turn: 0}
 
-        if(!rooms[room]) rooms[room] = {players: []};
-
-        if(rooms[room].players.length >= 4){
-            socket.emit("roomFull");
-            return;
+        if (rooms[room].players.length >= 4) {
+            socket.emit("roomFull")
+            return
         }
 
         const player = {
             id: socket.id,
             name,
             color,
-            hype:0,
-            position:0,
-            skip:false
-        };
+            position: 0,
+            hype: 0,
+            skip: false
+        }
 
-        rooms[room].players.push(player);
+        rooms[room].players.push(player)
+        socket.join(room)
+        io.to(room).emit("updatePlayers", rooms[room].players)
+    })
 
-        socket.join(room);
+    socket.on("rollDice", room => {
+        const dice = Math.floor(Math.random() * 6) + 1
+        io.to(room).emit("diceResult", dice)
+    })
 
-        io.to(room).emit("updatePlayers", rooms[room].players);
-    });
+    socket.on("move", data => {
+        const {room, playerId, position} = data
+        const player = rooms[room].players.find(p => p.id === playerId)
+        if (player) player.position = position
+        io.to(room).emit("updatePlayers", rooms[room].players)
+    })
 
-    socket.on("rollDice",(room)=>{
+    socket.on("disconnecting", () => {
+        for (let room in rooms) {
+            rooms[room].players = rooms[room].players.filter(p => p.id !== socket.id)
+            io.to(room).emit("updatePlayers", rooms[room].players)
+        }
+    })
 
-        const dice = Math.floor(Math.random()*6)+1;
+})
 
-        io.to(room).emit("diceResult",dice);
-
-    });
-
-});
-http.listen(3000,()=>console.log("Server started"));
+const PORT = process.env.PORT || 3000
+http.listen(PORT, () => console.log("Server started on port", PORT))
