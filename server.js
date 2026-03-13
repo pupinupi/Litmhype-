63e71ddd36015f1f664bc3d60323a0296caa536d
pupinupi <cool.lg-lg@yandex.ru>
2026-03-13 15:29:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,29 +10,25 @@ const wss = new WebSocket.Server({ server })
 
 let rooms = {}
 
-function broadcast(room, data){
+function broadcast(room,data){
 rooms[room].players.forEach(p=>{
 p.ws.send(JSON.stringify(data))
 })
 }
 
-wss.on("connection", ws=>{
+wss.on("connection",ws=>{
 
-ws.on("message", message=>{
+ws.on("message",msg=>{
 
-const data = JSON.parse(message)
+const data=JSON.parse(msg)
 
 if(data.type==="join"){
 
 if(!rooms[data.room]){
-rooms[data.room] = {
-players:[],
-started:false,
-turn:0
-}
+rooms[data.room]={players:[],turn:0,started:false}
 }
 
-const room = rooms[data.room]
+const room=rooms[data.room]
 
 if(room.players.length>=4){
 ws.send(JSON.stringify({type:"full"}))
@@ -60,18 +56,14 @@ color:p.color,
 hype:p.hype
 }))
 })
-
 }
 
 if(data.type==="start"){
 
-const room = rooms[data.room]
+const room=rooms[data.room]
 
 if(room.players.length<2){
-ws.send(JSON.stringify({
-type:"error",
-msg:"Нужно минимум 2 игрока"
-}))
+ws.send(JSON.stringify({type:"error"}))
 return
 }
 
@@ -81,14 +73,56 @@ broadcast(data.room,{
 type:"startGame",
 turn:room.players[0].name
 })
-
 }
 
-if(data.type==="move"){
+if(data.type==="dice"){
 
-const room = rooms[ws.room]
+const room=rooms[ws.room]
 
-const player = room.players.find(p=>p.name===ws.name)
+const player=room.players.find(p=>p.name===ws.name)
 
 player.pos=data.pos
-player
+player.hype=data.hype
+
+broadcast(ws.room,{
+type:"move",
+name:player.name,
+pos:player.pos,
+hype:player.hype,
+color:player.color
+})
+
+room.turn++
+
+if(room.turn>=room.players.length){
+room.turn=0
+}
+
+broadcast(ws.room,{
+type:"turn",
+player:room.players[room.turn].name
+})
+}
+
+})
+
+ws.on("close",()=>{
+
+const room=rooms[ws.room]
+
+if(!room) return
+
+room.players=room.players.filter(p=>p.name!==ws.name)
+
+broadcast(ws.room,{
+type:"players",
+players:room.players.map(p=>({
+name:p.name,
+color:p.color,
+hype:p.hype
+}))
+})
+})
+})
+
+server.listen(process.env.PORT||3000)
