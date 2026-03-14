6d914fcefe634b6ee38039f9d09432b075184946
pupinupi <cool.lg-lg@yandex.ru>
2026-03-14 11:07:31 +0300

    Create lobby.js

---
+++ public/lobby.js
@@ -0,0 +1,68 @@
+const socket=new WebSocket(location.origin.replace("http","ws"))
+
+let color=null
+
+document.querySelectorAll(".color").forEach(c=>{
+
+c.onclick=()=>{
+
+document.querySelectorAll(".color").forEach(x=>x.classList.remove("selected"))
+
+c.classList.add("selected")
+
+color=c.dataset.color
+
+}
+
+})
+
+document.getElementById("join").onclick=()=>{
+
+const name=document.getElementById("name").value
+const room=document.getElementById("room").value
+
+localStorage.setItem("name",name)
+localStorage.setItem("room",room)
+localStorage.setItem("color",color)
+
+socket.send(JSON.stringify({
+type:"join",
+name:name,
+room:room,
+color:color
+}))
+
+}
+
+document.getElementById("start").onclick=()=>{
+
+socket.send(JSON.stringify({
+type:"start",
+room:localStorage.getItem("room")
+}))
+
+}
+
+socket.onmessage=e=>{
+
+const data=JSON.parse(e.data)
+
+if(data.type==="players"){
+
+let html="<h3>Игроки</h3>"
+
+data.players.forEach(p=>{
+html+=`<div style="color:${p.color}">${p.name}</div>`
+})
+
+document.getElementById("players").innerHTML=html
+
+}
+
+if(data.type==="startGame"){
+
+location="room.html"
+
+}
+
+}
